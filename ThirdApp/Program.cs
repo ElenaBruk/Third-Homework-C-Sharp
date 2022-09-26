55471a2ad4249ed34c8b3bb94dc65ee33f18a144
Elena Bruk <aen_law@mail.ru>
2022-09-26 18:58:15 +0300

    Task 23

---
+++ ThirdApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int GetNumber()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void GetPow(int number)
+{
+    Console.WriteLine("Таблица кубов от 1 до " + number + ": ");
+    int count = 1;
+    while (count <= number)
+    {
+        Console.WriteLine(Math.Pow(count, 3));
+        count++;
+    }
+}
+GetPow(GetNumber());
